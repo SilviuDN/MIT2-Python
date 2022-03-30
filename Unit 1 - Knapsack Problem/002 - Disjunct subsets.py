7a4488c43b9bf9c6ed129a5fb1ced7b66b024272
SilviuDN <silviudilimot@gmail.com>
2022-03-30 21:44:39 +0200

    Yield disjoint subsets

--- Unit 1 - Knapsack Problem/002 - Disjunct subsets.py
+++ Unit 1 - Knapsack Problem/002 - Disjunct subsets.py
@@ -30,3 +30,22 @@ generator = powerSet(items)
 # =============================================================================
 
 
+#yield all pairs of disjunctive subsets
+def yieldAllCombos(items):
+    N = len(items)
+    # enumerate the 2**N possible combinations
+    for i in range(2**N):
+        combo = []
+        for j in range(N):
+            # test bit jth of integer i
+            if (i >> j) % 2 == 1:
+                combo.append(items[j])
+        #print( '************************************' )
+        #print(combo)
+        for m in range( len(combo) + 1 ):
+            tempTuple = (combo[:m], combo[m:] )
+            yield tempTuple 
+            #print(tempTuple )
+                
+generator2 = yieldAllCombos(items)
+
