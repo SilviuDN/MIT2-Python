f69ac176c48db5337a697c9a6978944d60c9e243
SilviuDN <silviudilimot@gmail.com>
2022-04-29 04:11:31 +0200

    handouts

--- Unit 3 - Inferential Statistics and Probability/0803 - Monte Cristo simulation.py
+++ Unit 3 - Inferential Statistics and Probability/0803 - Monte Cristo simulation.py
@@ -7,6 +7,27 @@ Created on Thu Apr 28 04:07:07 2022
 
 import random
 
+def noReplacementSimulation(numTrials):
+    '''
+    Runs numTrials trials of a Monte Carlo simulation
+    of drawing 3 balls out of a bucket containing
+    3 red and 3 green balls. Balls are not replaced once
+    drawn. Returns the a decimal - the fraction of times 3 
+    balls of the same color were drawn.
+    '''
+    # Your code here
+    counter = 0
+    for i in range(numTrials):
+        bucket = ['R', 'R', 'R', 'G', 'G', 'G']
+        picks = []
+        for j in range(3):
+            k = random.choice(bucket)
+            picks.append(k)
+            bucket.remove(k)
+        if picks[0] == picks[1] == picks[2]:
+            counter += 1
+    return counter/numTrials  
+
 # Paste your code here
 def drawing_without_replacement_sim(numTrials):
     '''
